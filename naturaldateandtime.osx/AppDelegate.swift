37dba6bcc08aa1829bb8ed56b314cef7c323cbae
Darko Sancanin <darkosancanin@mbp.local>
2015-07-31 21:51:28 +1000

    Added NSPopover with Question View and controller.

--- naturaldateandtime.osx/AppDelegate.swift
+++ naturaldateandtime.osx/AppDelegate.swift
@@ -5,14 +5,46 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
-
+    let popover = NSPopover()
+    var eventMonitor: EventMonitor?
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         if let button = statusItem.button {
             button.image = NSImage(named: "StatusBarIcon")
+            button.action = Selector("togglePopover:")
+        }
+        
+        popover.contentViewController = QuestionViewController(nibName: "QuestionView", bundle: nil)
+        
+        eventMonitor = EventMonitor(mask: .LeftMouseDownMask | .RightMouseDownMask) { [unowned self] event in
+            if self.popover.shown {
+                self.closePopover(event)
+            }
+        }
+        eventMonitor?.start()
+    }
+    
+    func applicationWillTerminate(aNotification: NSNotification) {
+    }
+    
+    func togglePopover(sender: AnyObject?) {
+        if popover.shown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
         }
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
+    
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSMinYEdge)
+        }
+        eventMonitor?.start()
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+        eventMonitor?.stop()
     }
 }
 
---
+++ naturaldateandtime.osx/QuestionViewController.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+class QuestionViewController: NSViewController {
+
+    @IBOutlet weak var testLabel: NSTextField!
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+    }
+}

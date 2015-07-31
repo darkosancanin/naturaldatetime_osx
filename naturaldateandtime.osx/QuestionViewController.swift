caad61abdc48a89541f833906bb67e779b8c5a53
Darko Sancanin <darkosancanin@mbp.local>
2015-07-31 22:10:27 +1000

    Added autogrowing NSTextField.

--- naturaldateandtime.osx/QuestionViewController.swift
+++ naturaldateandtime.osx/QuestionViewController.swift
@@ -1,8 +1,6 @@
 import Cocoa
 
 class QuestionViewController: NSViewController {
-
-    @IBOutlet weak var testLabel: NSTextField!
     
     override func viewWillAppear() {
         super.viewWillAppear()

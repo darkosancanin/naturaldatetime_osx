48325f1e279f0da3076b08b3f736cfcbaed7350c
Darko Sancanin <darkosancanin@mbp.local>
2015-08-01 22:14:28 +1000

    Cleaned up commented code.

--- naturaldateandtime.osx/QuestionViewController.swift
+++ naturaldateandtime.osx/QuestionViewController.swift
@@ -2,7 +2,6 @@ import Cocoa
 
 class QuestionViewController: NSViewController, NSTextFieldDelegate {
     
-    var isShowingSampleQuestion = true
     @IBOutlet var mainView: NSView!
     @IBOutlet weak var questionTextField: NSTextField!
     @IBOutlet weak var outerQuestionTextHeightConstraint: NSLayoutConstraint!
@@ -25,16 +24,6 @@ class QuestionViewController: NSViewController, NSTextFieldDelegate {
     override func controlTextDidChange(obj: NSNotification) {
         self.hideAll()
         self.questionTextField.textColor = NSColor.blackColor()
-        //if self.isShowingSampleQuestion {
-        //    self.isShowingSampleQuestion = false
-        //    self.questionTextField.stringValue = ""
-        //}
-        //else if self.questionTextField.stringValue.isEmpty {
-        //    self.showRandomQuestion()
-        //}
-        //if self.questionTextField.stringValue.isEmpty {
-        //    self.showRandomQuestion()
-        //}
         self.resizeQuestionTextField()
     }
  
@@ -44,7 +33,6 @@ class QuestionViewController: NSViewController, NSTextFieldDelegate {
         if self.questionTextField.stringValue.isEmpty {
             if let text = self.questionTextField.placeholderString {
                 textToSize = text
-                println("using placeholder text")
             }
         }
         let size = NSString(string: textToSize as NSString).boundingRectWithSize(constraint, options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: [NSFontAttributeName: self.questionTextField.font!])
@@ -54,15 +42,12 @@ class QuestionViewController: NSViewController, NSTextFieldDelegate {
     }
     
     @IBAction func questionTextFieldEnterPressed(sender: AnyObject) {
-        println("enter pressed")
+        
     }
     
     func showPlaceholderExampleQuestion() {
         self.questionTextField.stringValue = ""
         self.questionTextField.placeholderString = "e.g. " + ExampleQuestions.sharedInstance.getRandomQuestion()
-        //self.isShowingSampleQuestion = true
-        //self.questionTextField.selectText(self)
-        //self.questionTextField.currentEditor()!.moveToBeginningOfDocument(nil)
         self.resizeQuestionTextField()
     }
     

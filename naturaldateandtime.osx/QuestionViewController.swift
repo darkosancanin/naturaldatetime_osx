84a89a4ee3cefbf9c1a539d2fc5adddac7541ba6
Darko Sancanin <darkosancanin@mbp.local>
2015-08-01 22:09:59 +1000

    Fixed auto growing question box, not using TSTTextGrowth.

--- naturaldateandtime.osx/QuestionViewController.swift
+++ naturaldateandtime.osx/QuestionViewController.swift
@@ -2,8 +2,11 @@ import Cocoa
 
 class QuestionViewController: NSViewController, NSTextFieldDelegate {
     
+    var isShowingSampleQuestion = true
+    @IBOutlet var mainView: NSView!
     @IBOutlet weak var questionTextField: NSTextField!
     @IBOutlet weak var outerQuestionTextHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var questionTextHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var answerTextField: NSTextField!
     @IBOutlet weak var notesTextField: NSTextField!
     @IBOutlet weak var progressIndicator: NSProgressIndicator!
@@ -11,18 +14,62 @@ class QuestionViewController: NSViewController, NSTextFieldDelegate {
     override func viewWillAppear() {
         super.viewWillAppear()
         self.questionTextField.delegate = self
+        self.showPlaceholderExampleQuestion()
     }
     
     @IBAction func clearQuestionButtonPressed(sender: AnyObject) {
-        self.questionTextField.stringValue = ""
-        self.resizeQuestionTextField()
+        self.hideAll()
+        self.showPlaceholderExampleQuestion()
     }
     
     override func controlTextDidChange(obj: NSNotification) {
+        self.hideAll()
+        self.questionTextField.textColor = NSColor.blackColor()
+        //if self.isShowingSampleQuestion {
+        //    self.isShowingSampleQuestion = false
+        //    self.questionTextField.stringValue = ""
+        //}
+        //else if self.questionTextField.stringValue.isEmpty {
+        //    self.showRandomQuestion()
+        //}
+        //if self.questionTextField.stringValue.isEmpty {
+        //    self.showRandomQuestion()
+        //}
+        self.resizeQuestionTextField()
+    }
+ 
+    func resizeQuestionTextField(){
+        let constraint = CGSize(width: self.questionTextField.frame.width, height: CGFloat.max)
+        var textToSize = self.questionTextField.stringValue
+        if self.questionTextField.stringValue.isEmpty {
+            if let text = self.questionTextField.placeholderString {
+                textToSize = text
+                println("using placeholder text")
+            }
+        }
+        let size = NSString(string: textToSize as NSString).boundingRectWithSize(constraint, options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: [NSFontAttributeName: self.questionTextField.font!])
+        self.outerQuestionTextHeightConstraint.constant = size.height + 20
+        self.questionTextHeightConstraint.constant = size.height
+        //self.outerQuestionTextHeightConstraint.constant = self.questionTextField.intrinsicContentSize.height + 20
+    }
+    
+    @IBAction func questionTextFieldEnterPressed(sender: AnyObject) {
+        println("enter pressed")
+    }
+    
+    func showPlaceholderExampleQuestion() {
+        self.questionTextField.stringValue = ""
+        self.questionTextField.placeholderString = "e.g. " + ExampleQuestions.sharedInstance.getRandomQuestion()
+        //self.isShowingSampleQuestion = true
+        //self.questionTextField.selectText(self)
+        //self.questionTextField.currentEditor()!.moveToBeginningOfDocument(nil)
         self.resizeQuestionTextField()
     }
     
-    func resizeQuestionTextField(){
-        self.outerQuestionTextHeightConstraint.constant = self.questionTextField.intrinsicContentSize.height + 20
+    func hideAll() {
+        self.answerTextField.hidden = true
+        self.progressIndicator.hidden = true
+        self.progressIndicator.stopAnimation(self)
+        self.notesTextField.hidden = true
     }
 }

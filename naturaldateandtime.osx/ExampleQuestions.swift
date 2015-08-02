351393be22e39e2775868817c607b3ff53795813
Darko Sancanin <darkosancanin@mbp.local>
2015-08-02 19:08:49 +1000

    Updated examples.

--- naturaldateandtime.osx/ExampleQuestions.swift
+++ naturaldateandtime.osx/ExampleQuestions.swift
@@ -17,9 +17,9 @@ class ExampleQuestions {
 	
 	init() {
 		self.exampleQuestionsSections.append(ExampleQuestionsSection(sectionTitle: "Current time in a city", questions: ["What is the time in New York", "Time in Belgrade"]))
-		self.exampleQuestionsSections.append(ExampleQuestionsSection(sectionTitle: "Time conversions between cities in different timezones", questions: ["What is the time in Kathmandu, Nepal when it is 7pm in New York, USA", "If its 4pm in London what is the time in Boston"]))
-		self.exampleQuestionsSections.append(ExampleQuestionsSection(sectionTitle: "Time conversions between cities in different timezones on a particular date", questions: ["What is the time in Sao Paolo when it is 2:30pm on the 2nd of June in Detroit", "What time is it in Paris when it is 5-April-2015 in Sydney at 2am"]))
-		self.exampleQuestionsSections.append(ExampleQuestionsSection(sectionTitle: "Daylight saving information for a city", questions: ["When does daylight saving time begin in Auckland"]))
+		self.exampleQuestionsSections.append(ExampleQuestionsSection(sectionTitle: "Time conversions between cities in different timezones", questions: ["What is the time in Kathmandu, Nepal when it is 7pm in New York, USA", "Whats the time in Chicago when its 8pm Eastern Time"]))
+		self.exampleQuestionsSections.append(ExampleQuestionsSection(sectionTitle: "Time conversions between cities in different timezones on a particular date", questions: ["When its 8pm on the 9th of September Pacific Time, whats the time in Miami", "What time is it in Paris when it is 5-April-2015 in Sydney at 2am"]))
+		self.exampleQuestionsSections.append(ExampleQuestionsSection(sectionTitle: "Daylight saving information for a city", questions: ["When does daylight saving time begin in Auckland", "DST time in London"]))
 		self.exampleQuestionsSections.append(ExampleQuestionsSection(sectionTitle: "Daylight saving information for a city for a specific year", questions: ["When does daylight saving time begin in New York in 2016"]))
     }
 	
